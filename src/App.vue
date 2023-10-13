2b31493f68ad17aef6c138eb5079fd2a6cf01d03
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2023-10-13 18:32:24 +0700

    feat: add change key case

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { camelCase, constantCase, kebabCase, pascalCase, snakeCase } from 'change-case'
 import BaseButton from '@/components/BaseButton.vue'
 import BaseAlert from '@/components/BaseAlert.vue'
 
@@ -8,19 +9,58 @@ const baseModel = ref<string>('')
 const resultModel = ref<string>('')
 const alertText = ref<string>('')
 const alertType = ref<string>('')
-const isUsingSingleQuote = ref<boolean>(true)
+const quoteType = ref<string>('double')
+const caseType = ref<string>('')
 const space = ref<number>(2)
+
+function changeCase(obj, caseType) {
+  const keys = Object.keys(obj);
+
+  for (const key of keys) {
+    const newKey = (() => {
+      switch (caseType) {
+        case 'snake':
+          return snakeCase(key);
+        case 'kebab':
+          return kebabCase(key);
+        case 'camel':
+          return camelCase(key);
+        case 'pascal':
+          return pascalCase(key);
+        case 'constant':
+          return constantCase(key);
+        default:
+          return key;
+      }
+    })();
+
+    if (newKey !== key) {
+      obj[newKey] = obj[key];
+      delete obj[key];
+    }
+
+    if (typeof obj[newKey] === "object") {
+      changeCase(obj[newKey], caseType);
+    }
+  }
+  return obj;
+}
+
+function recursiveIterate(obj, caseType) {
+  changeCase(obj, caseType);
+}
 
 const convert = () => {
   try {
     alertType.value = ''
     const parsed = JSON.parse(baseModel.value)
+    recursiveIterate(parsed, caseType.value)
     const stringify = JSON.stringify(parsed, null, space.value)
 
     // remove doublequote from properties
     let unquote = stringify.replace(/"([^"]+)":/g, '$1:')
 
-    if (isUsingSingleQuote.value) {
+    if (quoteType.value === 'single') {
       // change " to '
       unquote = unquote.replace(/\"/g, "'")
     }
@@ -41,34 +81,68 @@ const copyToClipboard = () => {
     alertType.value = 'error'
   })
 }
+
+/**
+
+
+{
+  "key": "value",
+  "key space": "value",
+  "key-kebab": "value",
+  "key value": {
+    "key": "value",
+    "key space": "value",
+    "key-kebab": "value"
+  },
+  "key array": [ "value", "value", "value" ],
+  "key array object": [ { "key": "value" }, { "key": "value" }, { "key": "value" } ],
+  "key array object array": [ { "key": "value" }, { "key": "value" }, { "key": "value" } ],
+  "key array object array object": [ { "key": "value" }, { "key": "value" }, { "key": "value" } ],
+  "key array object array object array": [ { "key": {
+    "key array object array object array": [ { "key": {
+    "key array object array object array": [ { "key": {
+    "array": [1,2,3]
+  } }, { "key": "value" }, { "key": "value" } ]
+  } }, { "key": "value" }, { "key": "value" } ]
+  } }, { "key": "value" }, { "key": "value" } ]
+}
+
+ */
 </script>
 
 <template>
   <header>
     <h1 class="text-align-center">JSON to Javascript Converter</h1>
   </header>
-  <main class="main-area">
-    <textarea
-      id="base"
-      v-model="baseModel"
-      placeholder="write something here..."
-    />
-    <div class="options-area">
-      <base-alert v-if="alertType" :variant="alertType">
-        {{ alertText }}
-      </base-alert>
-      <div>
-        <label for="indent">Indent spacing?</label>
-        <input id="indent" type="number" v-model="space" class="input-indent" />
-      </div>
-      <label for="doubletick" class="mt-8">
-        <input id="doubletick" v-model="isUsingSingleQuote" type="checkbox" />
-        Convert Doublequote (") to Singlequote (') ?
-      </label>
+
+  <!-- OPTIONS -->
+  <div class="options-area">
+    <div class="option-item">
+      <label for="indent">Indent spacing?</label>
+      <input id="indent" type="number" v-model="space" />
+    </div>
+    <div class="option-item">
+      <label for="quote">Quote</label>
+      <select id="quote" v-model="quoteType">
+        <option value="single">Singlequote</option>
+        <option value="double">Doublequote</option>
+      </select>
+    </div>
+    <div class="option-item">
+      <label for="quote">Case</label>
+      <select id="quote" v-model="caseType">
+        <option value="">default</option>
+        <option value="snake">snake_case</option>
+        <option value="kebab">kebab-case</option>
+        <option value="camel">camelCase</option>
+        <option value="pascal">PascalCase</option>
+        <option value="constant">CONSTANT_CASE</option>
+      </select>
+    </div>
+    <div class="buttons">
       <base-button
         id="convert-text"
         class="mt-8"
-        block
         @click="convert"
       >
         Convert
@@ -76,13 +150,23 @@ const copyToClipboard = () => {
       <base-button
         id="copy-text"
         class="mt-8"
-        block
         :disabled="!resultModel"
         @click="copyToClipboard"
       >
         Copy Result
       </base-button>
     </div>
+    <base-alert v-if="alertType" :variant="alertType" class="mt-16">
+      {{ alertText }}
+    </base-alert>
+  </div>
+
+  <main class="main-area mt-16">
+    <textarea
+      id="base"
+      v-model="baseModel"
+      placeholder="write something here..."
+    />
     <textarea
       id="result"
       v-model="resultModel"
@@ -105,18 +189,35 @@ textarea {
   text-align: center;
 }
 
-.main-area {
-  width: 80vw;
-  height: 80vh;
-  display: grid;
-  grid-template-columns: 1fr;
-}
-
 .options-area {
+  margin: 0 auto;
   padding: 16px;
   display: flex;
   flex-direction: column;
   order: 1;
+  background: white;
+  color: black;
+  width: max-content;
+  border-radius: 8px;
+}
+
+.option-item {
+  width: 500px;
+  display: grid;
+  grid-template-columns: 200px 300px;
+  margin-bottom: 8px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+}
+
+.main-area {
+  display: grid;
+  grid-template-columns: 1fr;
+  min-height: 450px;
 }
 
 .mt-8 {
@@ -127,19 +228,15 @@ textarea {
   margin-top: 16px;
 }
 
-.input-indent {
-  margin-left: 8px;
-  width: 100px;
-}
-
 .author {
   font-weight: 800;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 1200px) {
   .main-area {
-    grid-template-columns: 1fr 250px 1fr;
-    height: 80vh;
+    width: 1024px;
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
   }
 
   .options-area {
